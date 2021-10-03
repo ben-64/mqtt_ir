01af616458c204790eec4414de9bda014f2f803e
ben64 <ben64@time0ut.org>
2021-10-03 15:25:20 +0200

    tab -> space

--- mqtt_ir.go
+++ mqtt_ir.go
@@ -3,8 +3,8 @@ package main
 import (
     "fmt"
     mqtt "github.com/eclipse/paho.mqtt.golang"
-	"github.com/ilyakaznacheev/cleanenv"
-	"github.com/chbmuc/lirc"
+    "github.com/ilyakaznacheev/cleanenv"
+    "github.com/chbmuc/lirc"
     "time"
     "os"
     "os/signal"
@@ -12,16 +12,16 @@ import (
 )
 
 type ConfigDatabase struct {
-	// MQTT Configuration
+    // MQTT Configuration
     ServerAddress     string `yaml:"server_adress" env:"MQTT_SERVER_ADDRESS" env-default:"tcp://localhost:1883"`
     User              string `yaml:"user" env:"MQTT_USER" env-default:"user"`
     Password          string `yaml:"password" env:"MQTT_PASSWORD" env-default:"password"`
-	ClientID          string `yaml:"clientid" env:"MQTT_CLIENTID" env-default:"ir_client"`
-	Topic             string `yaml:"topic" env:"MQTT_TOPIC" env-default:"topic"`
+    ClientID          string `yaml:"clientid" env:"MQTT_CLIENTID" env-default:"ir_client"`
+    Topic             string `yaml:"topic" env:"MQTT_TOPIC" env-default:"topic"`
 
-	// Lirc Configuration
-	LircSocket		  string `yaml:"lirc_socket" env:"LIRC_SOCKET" env-default:"/var/run/lirc/lircd"`
-	LircDevice		  string `yaml:"lirc_device" env:"LIRC_DEVICE" env-default:"lircdevice"`
+    // Lirc Configuration
+    LircSocket          string `yaml:"lirc_socket" env:"LIRC_SOCKET" env-default:"/var/run/lirc/lircd"`
+    LircDevice          string `yaml:"lirc_device" env:"LIRC_DEVICE" env-default:"lircdevice"`
 }
 
 var cfg ConfigDatabase
@@ -30,16 +30,16 @@ var IR *lirc.Router
 
 func handle_incoming_mqtt_message(client mqtt.Client, msg mqtt.Message) {
     fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
-	err := IR.Send(fmt.Sprintf("%s %s",cfg.LircDevice,msg.Payload()))
-	if err != nil {
-		fmt.Printf("Error sending LIRC data: %+v\n",err)
-	}
+    err := IR.Send(fmt.Sprintf("%s %s",cfg.LircDevice,msg.Payload()))
+    if err != nil {
+        fmt.Printf("Error sending LIRC data: %+v\n",err)
+    }
 }
 
 
 // Create MQTT Client
 func create_mqtt_client(conf *ConfigDatabase) mqtt.Client {
-	var mqtt_qos byte = 1
+    var mqtt_qos byte = 1
 
     // Now we establish the connection to the mqtt broker
     opts := mqtt.NewClientOptions()
@@ -69,7 +69,7 @@ func create_mqtt_client(conf *ConfigDatabase) mqtt.Client {
     }
 
     client := mqtt.NewClient(opts)
-	client.AddRoute(conf.Topic,handle_incoming_mqtt_message)
+    client.AddRoute(conf.Topic,handle_incoming_mqtt_message)
     if token := client.Connect(); token.Wait() && token.Error() != nil {
         panic(token.Error())
     }
@@ -82,21 +82,21 @@ func create_mqtt_client(conf *ConfigDatabase) mqtt.Client {
 
 // Create IR Client
 func create_ir_client(conf *ConfigDatabase) {
-	// Initialize with path to lirc socket
-	var err error
-	IR, err = lirc.Init(conf.LircSocket)
-	if err != nil {
-		panic(err)
-	}
+    // Initialize with path to lirc socket
+    var err error
+    IR, err = lirc.Init(conf.LircSocket)
+    if err != nil {
+        panic(err)
+    }
 }
 
 func main() {
-	err := cleanenv.ReadConfig("config.yml", &cfg)
-	if err != nil {
-		fmt.Printf("Unable to open %s configuration file\n","config.yml")
-	}
+    err := cleanenv.ReadConfig("config.yml", &cfg)
+    if err != nil {
+        fmt.Printf("Unable to open %s configuration file\n","config.yml")
+    }
     client := create_mqtt_client(&cfg)
-	create_ir_client(&cfg)
+    create_ir_client(&cfg)
 
     // Messages will be delivered asynchronously so we just need to wait for a signal to shutdown
     sig := make(chan os.Signal, 1)
@@ -104,7 +104,7 @@ func main() {
     signal.Notify(sig, syscall.SIGTERM)
 
     <-sig
-	fmt.Println("signal caught - exiting")
+    fmt.Println("signal caught - exiting")
     client.Disconnect(1000)
 }
 
