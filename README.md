114446f3a38aa466548f3f7d92ab7ea445e744a1
ben64 <ben64@time0ut.org>
2021-10-16 19:09:38 +0200

    Implementing a low level mode, where lircd is not anymore mandatory

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Introduction
 
-This program is waiting for an id button on a `MQTT` topic and execute the corresponding `LIRC` command.
+This program can be used in two different modes:
+- Either it is waiting for an id button on a `MQTT` topic and execute the corresponding `LIRC` command, by communicating with the `lircd` unix socket
+- Or it is waiting for an IR command, and send it directly to the IR driver, bypassing `lircd`
 
-# Configuration
+# Configuration to use `lircd`
 
 This program can be configured through a configuration file called `config.yml` in the same folder.
 
@@ -22,9 +24,40 @@ clientid: ir_mqtt
 # Topic to subscribe to
 topic: ir
 
+# High level mode, meaning that we are going to use lircd
+mode: high
+
 # Unix socket to communicate with the LIRC daemon
 lirc_socket: /var/run/lirc/lircd
 
 # Device for LIRC communication
 lirc_device: aircooling
+```
+
+# Configuration for interfacing with the default IR driver
+
+```conf
+# Connection string for the MQTT server
+server_adress: tcp://mqtt_server:1883
+
+# User for MQTT connection
+user: mqtt_user
+
+# Password for MQTT connection
+password: mqtt_password
+
+# ID Client in MQTT communication
+clientid: ir_mqtt
+
+# Topic to subscribe to
+topic: ir
+
+# High level mode, meaning that we are going to use lircd
+mode: low
+
+# Frequency to give to the driver
+ir_frequency: 38000
+
+# Duty cycle for the driver
+ir_dutycycle: 50
 ```
--- mqtt_ir.go
+++ mqtt_ir.go
@@ -5,35 +5,134 @@ import (
     mqtt "github.com/eclipse/paho.mqtt.golang"
     "github.com/ilyakaznacheev/cleanenv"
     "github.com/chbmuc/lirc"
+    "errors"
     "time"
     "os"
     "os/signal"
+    "strconv"
     "syscall"
+    "encoding/binary"
+    "unsafe"
 )
 
 type ConfigDatabase struct {
     // MQTT Configuration
-    ServerAddress     string `yaml:"server_adress" env:"MQTT_SERVER_ADDRESS" env-default:"tcp://localhost:1883"`
-    User              string `yaml:"user" env:"MQTT_USER" env-default:"user"`
-    Password          string `yaml:"password" env:"MQTT_PASSWORD" env-default:"password"`
-    ClientID          string `yaml:"clientid" env:"MQTT_CLIENTID" env-default:"ir_client"`
-    Topic             string `yaml:"topic" env:"MQTT_TOPIC" env-default:"topic"`
+    ServerAddress       string `yaml:"server_adress" env:"MQTT_SERVER_ADDRESS" env-default:"tcp://localhost:1883"`
+    User                string  `yaml:"user" env:"MQTT_USER" env-default:"user"`
+    Password            string  `yaml:"password" env:"MQTT_PASSWORD" env-default:"password"`
+    ClientID            string  `yaml:"clientid" env:"MQTT_CLIENTID" env-default:"ir_client"`
+    Topic               string  `yaml:"topic" env:"MQTT_TOPIC" env-default:"topic"`
 
-    // Lirc Configuration
-    LircSocket          string `yaml:"lirc_socket" env:"LIRC_SOCKET" env-default:"/var/run/lirc/lircd"`
-    LircDevice          string `yaml:"lirc_device" env:"LIRC_DEVICE" env-default:"lircdevice"`
+    // Mode : High level or low level
+    Mode                string `yaml:"mode" env:"MQTT_IR_MODE" env-default:"low"`
+
+    // High level Lirc Configuration
+    LircSocket          string  `yaml:"lirc_socket" env:"LIRC_SOCKET" env-default:"/var/run/lirc/lircd"`
+    LircDevice          string  `yaml:"lirc_device" env:"LIRC_DEVICE" env-default:"lircdevice"`
+
+    // Low Level IR driver configuration
+    IRDriver            string `yaml:"ir_driver" env:"IR_DRIVER" env-default:"/dev/lirc0"`
+    IRFreq              int    `yaml:"ir_freq" env:"IR_FREQ" env-default:"38000"`
+    IRDutyCycle         int    `yaml:"ir_dutycycle" env:"IR_DUTYCYCLE" env-default:"50"`
 }
+
+// IOCTL values when communicating directly with the driver
+const (
+    LIRC_SET_SEND_CARRIER uint32    = 0x40046913
+    LIRC_SET_SEND_DUTY_CYCLE uint32 = 0x40046915
+)
 
 var cfg ConfigDatabase
 var IR *lirc.Router
 
 
 func handle_incoming_mqtt_message(client mqtt.Client, msg mqtt.Message) {
+    var err error
     fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
-    err := IR.Send(fmt.Sprintf("%s %s",cfg.LircDevice,msg.Payload()))
-    if err != nil {
-        fmt.Printf("Error sending LIRC data: %+v\n",err)
+    if cfg.Mode == "low" {
+        err = send_lowlevel_ir(msg.Payload(),&cfg)
+    } else {
+        err = IR.Send(fmt.Sprintf("%s %s",cfg.LircDevice,msg.Payload()))
     }
+    if err != nil {
+        fmt.Printf("Error sending IR data: %+v\n",err)
+    }
+}
+
+// Change a hexvalue string into a string representing
+// sequence of pulses/spaces understandable by the driver
+func from_hexvalue_to_buffer(value string) ([]byte,error) {
+    var pulses_spaces []uint32
+    var buffer []byte
+
+    // A
+    pulses_spaces = append(pulses_spaces,4350)
+    pulses_spaces = append(pulses_spaces,4350)
+
+    // Transform the HEX String value to a list of pulses and spaces
+    for i := 0; i < len(value); i += 2 {
+        x,err := strconv.ParseUint(value[i:i+2],16,8)
+        if err != nil {
+            fmt.Printf("Unable to decode hex string %s",value[i:i+1])
+            return nil,errors.New("unable hex string decode")
+        }
+
+        // Extract each bit from the string
+        // If 0 then (460,460), otherwise (460,1600)
+        for j := 0; j < 8; j++ {
+            pulses_spaces = append(pulses_spaces,460)
+            // if bit j is different from 0
+            if ((1<<uint(7-j)) & x) != 0 {
+                pulses_spaces = append(pulses_spaces,1600)
+            } else {
+                pulses_spaces = append(pulses_spaces,600)
+            }
+        }
+    }
+
+    // Transform the list of pulses/spaces in a byte array
+    for i:=0; i<len(pulses_spaces); i+=2 {
+        bs := make([]byte, 8)
+        binary.LittleEndian.PutUint32(bs[:4], pulses_spaces[i])
+        binary.LittleEndian.PutUint32(bs[4:], pulses_spaces[i+1])
+        buffer = append(buffer,bs...)
+    }
+    buffer = append(buffer,0xCA,0x01,0x00,0x00)
+
+    return buffer,nil
+}
+
+// Send the hexvalue string as a IR signal
+func send_lowlevel_ir(value []byte, conf *ConfigDatabase) error {
+    // Initialize driver
+    fd, err := syscall.Open(conf.IRDriver, syscall.O_RDWR, 0666)
+    if err != nil {
+        return errors.New(fmt.Sprintf("Unable to open %s : %+v\n", conf.IRDriver, err))
+    }
+
+    _, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd), uintptr(LIRC_SET_SEND_CARRIER), uintptr(unsafe.Pointer(&cfg.IRFreq)))
+    if errno != 0 {
+        return errors.New(fmt.Sprintf("Unable to ioctl LIRC_SET_SEND_CARRIER : %+v", err))
+    }
+    _, _, errno = syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd), uintptr(LIRC_SET_SEND_DUTY_CYCLE), uintptr(unsafe.Pointer(&cfg.IRDutyCycle)))
+    if errno != 0 {
+        return errors.New(fmt.Sprintf("Unable to ioctl LIRC_SET_SEND_DUTY_CYCLE : %+v", errno))
+    }
+
+    // Build buffer bytes
+    buf,err := from_hexvalue_to_buffer(string(value))
+    if err != nil {
+        return errors.New(fmt.Sprintf("Unable to unpack hex value string"))
+    }
+
+    _,err = syscall.Write(fd, buf)
+    if err != nil {
+        return errors.New(fmt.Sprintf("Unable to send to driver : %+v",err))
+    }
+
+    _ = syscall.Close(fd)
+
+    return nil
 }
 
 
@@ -95,8 +194,11 @@ func main() {
     if err != nil {
         fmt.Printf("Unable to open %s configuration file\n","config.yml")
     }
+
     client := create_mqtt_client(&cfg)
-    create_ir_client(&cfg)
+    if cfg.Mode != "low" {
+        create_ir_client(&cfg)
+    }
 
     // Messages will be delivered asynchronously so we just need to wait for a signal to shutdown
     sig := make(chan os.Signal, 1)
